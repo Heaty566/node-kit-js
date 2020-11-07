edea43137a84c3b13b573bc07f64f48e17910c33
heaty <heaty566@gmail.com>
2020-11-07 21:51:05 +0700

    add run

--- src/app/router.js
+++ src/app/router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
 const store = new MongoDBStore({
@@ -14,8 +16,9 @@ module.exports = function (app) {
         app.use(express.json());
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(cors({ credentials: true }));
+        app.use(cookieParser());
         app.use(express.static(process.cwd() + "/public"));
-
+        app.use(morgan("dev"));
         app.use(
                 session({
                         secret: process.env.SESSION_SECRET,
@@ -31,7 +34,8 @@ module.exports = function (app) {
         app.use((req, res, next) => {
                 req.header("Access-Control-Allow-Origin", "*");
                 req.header("Access-Control-Allow-Headers", "*");
-                if (req.session.isUpdate === undefined) req.session.isUpdate = true;
+                if (req.session.isUpdate === undefined)
+                        req.session.isUpdate = true;
 
                 next();
         });
